56ad0c6305ca8ba4594fdfae2eac076e96b8b39d
RadkaBartonova <ba.radka@email.cz>
2025-05-17 15:23:17 +0200

    pridan novy soubor

---
+++ config.py
@@ -0,0 +1,16 @@
+# Hlavni database
+DB_CONFIG = {
+    'host': 'localhost',
+    'user': 'root',    # Změň na své údaje
+    'password': 'taskmanager',    # Změň na své údaje
+    'database': 'ukoly_db'
+}
+
+# Testovaci database
+# Testovací databáze
+TEST_DB_CONFIG = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'taskmanager',
+    'database': 'ukoly_db_test'
+}
